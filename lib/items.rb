7375efe29071a8f87a7c30edc0fc793e1b16e069
twofford <taylor.wofford@gmail.com>
2025-03-05 00:23:42 -0500

    Add price attribute and methods for price calculation in Item class

--- lib/items.rb
+++ lib/items.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Item
+  attr_reader :price
+
+  def set_price
+    @price = calculate_price
+  end
+
+  def calculate_price
+    raise NotImplementedError
+  end
 end
 
 class Armor < Item
