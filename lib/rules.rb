2b5813be9beea8d9bd42c0a63a6dce674f359fee
twofford <taylor.wofford@gmail.com>
2025-03-05 00:24:13 -0500

    Add cost constants for range limits, damage dice, crit multipliers, ammo quality, and reliability

--- lib/rules.rb
+++ lib/rules.rb
@@ -40,4 +40,55 @@ class Rules
     very_hard: 150,
     nigh_impossible: 300
   }.freeze
+
+  RANGE_LIMIT_COSTS = RANGE_LIMITS.values.each.with_object({}).with_index do |(v, obj), idx|
+    if v == RANGE_LIMITS.values.first
+      obj[0..v] = 0.75 * (idx + 1)
+      next
+    end
+
+    if v == RANGE_LIMITS.values.last
+      obj[v..Float::INFINITY] = 0.75 * (idx + 1)
+      next
+    end
+
+    obj[v..RANGE_LIMITS.values[idx + 1]] = 0.75 * (idx + 1)
+  end
+
+  DAMAGE_DICE_COSTS = {
+    D4: 20_000,
+    D6: 35_000,
+    D8: 50_000,
+    D10: 75_000,
+    D12: 120_000,
+    D20: 650_000
+  }.freeze
+
+  CRIT_MULTIPLIER_COSTS = {
+    2 => 1,
+    3 => 2,
+    4 => 3,
+    5 => 4,
+    6 => 5
+  }.freeze
+
+  AMMO_QUALITY_COSTS = {
+    -3 => 0.25,
+    -2 => 0.5,
+    -1 => 0.75,
+    0 => 1,
+    1 => 1.25,
+    2 => 1.5,
+    3 => 1.75
+  }.freeze
+
+  RELIABILITY_COSTS = {
+    -3 => 0.25,
+    -2 => 0.5,
+    -1 => 0.75,
+    0 => 1,
+    1 => 1.25,
+    2 => 1.5,
+    3 => 1.75
+  }.freeze
 end
