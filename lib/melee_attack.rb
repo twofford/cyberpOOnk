6c71b9a71b7dd98341174adc04a9d02d7bfd6f7a
Taylor Wofford <taylor.wofford@gmail.com>
2025-03-08 02:07:20 -0500

    Enhance MeleeAttack class with attacker and target attributes; implement initialization and make methods for attack calculation

--- lib/melee_attack.rb
+++ lib/melee_attack.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class MeleeAttack < Attack
+  attr_reader :attacker, :target, :weapon, :modifiers
+
+  def initialize(attacker:, target:, weapon:, modifiers: [])
+    super(
+      attacker: attacker,
+      target: target,
+      range_to_target: 0,
+      weapon: weapon,
+      modifiers: modifiers
+    )
+  end
+
+  def make
+    super
+
+    roll = D20.roll + attacker.public_send(stat) + attacker.public_send(skill) + modifiers.sum
+    calculate_degree_of_success(difficulty_level: :easy, roll: roll)
+  end
 end
