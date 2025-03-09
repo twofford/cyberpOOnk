0732a70959a2a43e2c7e4a942ec75f7a9effa20e
Taylor Wofford <taylor.wofford@gmail.com>
2025-03-09 01:51:34 -0500

    Remove unused difficulty level methods from RangedWeapon class

--- lib/ranged_weapon.rb
+++ lib/ranged_weapon.rb
@@ -7,46 +7,4 @@ class RangedWeapon < Weapon
     @max_range = max_range
     super(damage_attributes: damage_attributes, mods: mods)
   end
-
-  # def difficulty_level_for_target(range_to_target:)
-  #   difficulty_level_ranges.each do |dl, range|
-  #     return dl if range.include?(range_to_target)
-  #   end
-  #   raise UnkownDifficultyLevelError
-  # end
-
-  # def easy_range
-  #   1..Constants::RANGE_LIMITS[:easy]
-  # end
-
-  # def medium_range
-  #   (Constants::RANGE_LIMITS[:easy] + 1)..Constants::RANGE_LIMITS[:medium]
-  # end
-
-  # def hard_range
-  #   (Constants::RANGE_LIMITS[:medium] + 1)..Constants::RANGE_LIMITS[:hard]
-  # end
-
-  # def very_hard_range
-  #   (Constants::RANGE_LIMITS[:hard] + 1)..Constants::RANGE_LIMITS[:very_hard]
-  # end
-
-  # def nigh_impossible_range
-  #   (Constants::RANGE_LIMITS[:very_hard] + 1)..Constants::RANGE_LIMITS[:nigh_impossible]
-  # end
-
-  # def impossible_range
-  #   (Constants::RANGE_LIMITS[:nigh_impossible] + 1)..Float::INFINITY
-  # end
-
-  # def difficulty_level_ranges
-  #   {
-  #     easy: easy_range,
-  #     medium: medium_range,
-  #     hard: hard_range,
-  #     very_hard: very_hard_range,
-  #     nigh_impossible: nigh_impossible_range,
-  #     impossible: impossible_range
-  #   }
-  # end
 end
