ed094cf193b0df885641702e6b14e445bd7dc04d
Taylor Wofford <taylor.wofford@gmail.com>
2025-03-12 18:15:18 -0400

    Add cybernetics features and humanity cost details to game mechanics

--- cyberpoonk.rb
+++ cyberpoonk.rb
@@ -47,6 +47,47 @@ ammo = Ammo.new(damage_dice: D6, crit_multiplier: 2, quality: 0)
 gun = Gun.new(ammo: ammo, reliability: 0, max_range: 50)
 knife = MeleeWeapon.new(damage_attributes: DamageAttributes.new(damage_dice: D4, crit_multiplier: 2), mods: [])
 
-puts Attack.new(attacker: char, target: char, range_to_target: 10, weapon: gun, modifiers: []).make
-puts RangedAttack.new(attacker: char, target: char, range_to_target: 10, weapon: gun, modifiers: []).make
-puts MeleeAttack.new(attacker: char, target: char, weapon: knife, modifiers: []).make
+### Cybernetics
+
+## For every +1 to a stat or +2 to a skill, -1 humanity
+## For every +5 hp, -1 humanity
+
+## Full Body
+# Full Body Prosthesis - +10 brawn and reflexes, +50 hp, -30 humanity
+# Nanosurgeons 1 - 2x healing rate
+# Nanosurgeons 2 - 3x healing rate
+
+## Eyes
+# Targeting Optics - +2 marksmanship, -1 humanity
+# Full Spectrum Vision - see in all spectra of light, -1 humanity
+# Antidazzle - immune to blindness, -1 humanity
+# Coordination Suite - allows users to attack with more than one weapon without penalty, -4 humanity
+
+## Ears
+
+## Arms
+# Grafted Muscles - +2 brawn, -2 humanity, user can use two-handed weapon with one hand, can be taken twice
+# Cyberarm - +2 brawn, +5 hp, -3 humanity, user can use two-handed weapon with one hand, can be taken twice
+
+## Hands
+# Gunlink - +2 marksmanship, -1 humanity
+# Wolverine Claws - 2d6 damage x3 crit mult, -2 humanity
+
+## Legs
+# Grafted Muscles - +2 brawn, +10 speed, -2 humanity
+# Cyberleg - +2 brawn, +5 hp, -3 humanity
+
+## Feet
+
+## Anywhere
+# Snooper Shielding - snoopers don't work on cyberware in the body part
+
+## Brain
+# Reflex Booster 1 - +2 reflexes, +4 initiative, -2 humanity
+# Reflex Booster 2 - +2 reflexes, +8 initiative, -4 humanity
+# Reflex Booster 3 - +2 reflexes, +12 initiative, -6 humanity
+# Postmortem Puppeteer - when you reach 0 hp, you can keep fighting until the end of the encounter, but then you die -10 humanity
+
+## Skin
+# Subdermal Armor - +4 DR, -2 huamnity
+# Optical Camouflage - +6 stealth, -3 humanity (only helpful if you're naked)
